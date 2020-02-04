73cef13f87b8b4506a52fba0232bc09c993e1323
poloo987 <francoorantes12@gmail.com>
2020-02-04 15:59:32 -0500

    zero bank thing

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -1,4 +1,20 @@
 package com.zerobank.runners;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-cucumber-reports",
+                "rerun:target/rerun.txt"
+        },
+        features = "src/test/resources/features/",
+        glue = "com/vytrack/step_definitions",
+        dryRun = false,
+        tags = "@VYT-123"
+)
 public class CukesRunner {
 }
